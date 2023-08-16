788cf81957e30c68fc9fe0abb41ad50911b85c94
nelumbo-design-it <nelumbodesignit@gmail.com>
2023-08-16 22:02:42 +0200

    Aggiunte testimonianze e altri dettagli

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -7,6 +7,33 @@ import { Title, Meta } from '@angular/platform-browser';
 })
 export class HomeComponent implements OnInit {
 
+  public testimonials = [
+    {
+      recensione: 'Un team eccezionale! Hanno trasformato la mia idea in un sito web funzionale e accattivante.',
+      nome: 'Piero',
+      cognome: 'Rossi',
+      image: '../assets/images/testimonial/Piero_rossi.jpg'
+    },
+    {
+      recensione: 'Grazie alla loro strategia SEO, ho visto un aumento significativo del traffico sul mio sito.',
+      nome: 'Luca',
+      cognome: 'Bianchi',
+      image: '../assets/images/testimonial/Luca_bianchi.jpg'
+    },
+    {
+      recensione: 'Dall\'inizio alla fine, l\'esperienza è stata impeccabile. Il risultato finale parla da solo: un design pulito, funzionale e user-friendly. Non avrei potuto chiedere di meglio',
+      nome: 'Giulia',
+      cognome: 'Verdi',
+      image: '../assets/images/testimonial/Giulia_verdi.jpg'
+    },
+    {
+      recensione: 'Sono rimasta colpita dalla loro attenzione ai dettagli e dalla capacità di capire esattamente ciò di cui avevo bisogno. Un vero piacere lavorare con loro!',
+      nome: 'Francesca',
+      cognome: 'Neri',
+      image: '../assets/images/testimonial/Francesca_neri.jpg'
+    },
+  ];
+
   constructor(private titleService: Title, private metaService: Meta){}
 
   ngOnInit() {
--- src/app/shared/components/testimonial-card/testimonial-card.component.ts
+++ src/app/shared/components/testimonial-card/testimonial-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-testimonial-card',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./testimonial-card.component.scss']
 })
 export class TestimonialCardComponent {
-
+  @Input() recensione: string = '';
+  @Input() nome: string = '';
+  @Input() cognome: string = '';
+  @Input() image: string = '';
 }
