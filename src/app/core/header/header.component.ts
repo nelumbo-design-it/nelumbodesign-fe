34ff7e1552e255a2f4f9f88595b03a9c40bc58e6
nelumbo-design-it <nelumbodesignit@gmail.com>
2023-08-21 18:05:14 +0200

    Effetto header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import anime from 'animejs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  ngOnInit(): void {
+    this.animeJS();
+  }
+  animeJS() {
+    const pathEls: any = document.querySelectorAll('path');
+    for (let i = 0; i < pathEls.length; i++) {
+      const pathEl: any = pathEls[i];
+      const offset: any = anime.setDashoffset(pathEl);
+      pathEl.setAttribute('stroke-dashoffset', offset);
+      anime({
+        targets: pathEl,
+        strokeDashoffset: [offset, 0],
+        duration: anime.random(1000, 3000),
+        delay: anime.random(0, 2000),
+        loop: true,
+        direction: 'alternate',
+        easing: 'easeInOutSine',
+        autoplay: true,
+      });
+    }
+  }
 }
